9b0d92fe982b2d5797f532b83fc6eb240286e09e
Jaison Oenning <jaisonoenning_@hotmail.com>
2024-07-17 12:03:06 -0300

    Implementando as regras dos celulares

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; }
 
-        public Smartphone(string numero)
+        protected Smartphone(string numero, string modelo, string iMEI, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia01", iMEI: "542187446", memoria: 64);
+
+nokia.Ligar();
+Console.WriteLine("\n");
+
+nokia.ReceberLigacao();
+Console.WriteLine("\n");
+
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("--------------------");
+
+Console.WriteLine("Smarphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "456323", modelo: "13 ProMax", iMEI: "8764684314", memoria: 128);
+
+iphone.Ligar();
+Console.WriteLine("\n");
+
+iphone.ReceberLigacao();
+Console.WriteLine("\n");
+
+iphone.InstalarAplicativo("Telegram");
+
